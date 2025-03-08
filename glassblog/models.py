4c5ff319029314ddf7ec4664b85fdac4f60fbea1
Nikki <bogdnico@gmail.com>
2025-03-08 12:08:52 -0600

    Final Project

--- glassblog/models.py
+++ glassblog/models.py
@@ -1,12 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.utils.timezone import now
 
 class BlogPost(models.Model):
+    TOPIC_CHOICES = [
+        ('Daisy Pattern', 'Daisy Pattern'),
+        ('Dot Pattern', 'Dot Pattern'),
+        ('Primary Pattern', 'Primary Pattern'),
+        ('Other', 'Other'),
+    ]
     title = models.CharField(max_length=150)
     content = models.TextField()
     date_posted = models.DateTimeField(default=now)
     author = models.CharField(max_length=50)
-    topic = models.CharField(max_length=100) 
+    topic = models.CharField(max_length=100, choices=TOPIC_CHOICES, default='Other') 
     image = models.ImageField(upload_to='images/', blank=True, null=True)
 
     def __str__(self):
